fe2bc4fb3678221e9cea202104f1821bdb20b074
Katy <37582048+eswarakatyayani@users.noreply.github.com>
2025-08-24 18:48:05 +0530

    Update g_scratchpad.js

--- Chapter6/g_scratchpad.js
+++ Chapter6/g_scratchpad.js
@@ -1,1 +1,5 @@
-
+/*
+check BR+CS use case.
+A read-only JS object available on the client (browser) that carries data computed on the server during form load.
+It’s populated by a Display Business Rule (runs before the form renders) and then accessible in Client Scripts/UI Policies via g_scratchpad.someField
+*/
